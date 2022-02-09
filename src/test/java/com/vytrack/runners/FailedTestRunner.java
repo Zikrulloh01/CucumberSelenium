4e2cde14b45dd62c4e07489584b2c2a431cc6859
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-02-09 15:26:00 +0000

    FailedTestRunner class has been created

--- src/test/java/com/vytrack/runners/FailedTestRunner.java
+++ src/test/java/com/vytrack/runners/FailedTestRunner.java
@@ -8,6 +8,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
+        plugin = {"html:target/failed-html-report"},
+        features = "@target/rerun.txt",
+        glue = "com/vytrack/step_definitions"
 )
 public class FailedTestRunner {
 
